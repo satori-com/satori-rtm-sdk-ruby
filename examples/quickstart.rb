687651ca82531abe1431c4a2a9ed1ad84cce0a9c
Andrey Vasenin <avasenin@satori.com>
2017-09-04 17:29:38 +0200

    Update quickstart to follow template

--- examples/quickstart.rb
+++ examples/quickstart.rb
@@ -4,11 +4,25 @@ require 'satori-rtm-sdk'
 
 endpoint = 'YOUR_ENDPOINT'
 appkey = 'YOUR_APPKEY'
+# Role and role secret are optional: replace only if you need to authenticate.
 role = 'YOUR_ROLE'
 role_secret = 'YOUR_SECRET'
 
+# Check if the role is set to authenticate or not
+should_authenticate = (role != 'YOUR_ROLE')
+
+puts 'RTM client config:'
+puts "\tendpoint = #{endpoint}"
+puts "\tappkey = #{appkey}"
+if should_authenticate
+  puts "\tauthenticate? = true (as #{role})"
+else
+  puts "\tauthenticate? = false"
+end
+
 client = Satori::RTM::Client.new(endpoint, appkey)
 
+# Hook up to client connectivity state transitions
 client.onopen do
   puts 'Connected to Satori RTM!'
 end
@@ -23,7 +37,7 @@ end
 
 client.connect
 
-if role != 'YOUR_ROLE'
+if should_authenticate
   client.authenticate role, role_secret do |reply|
     raise "Failed to authenticate: #{reply.data[:error]} -- #{reply.data[:reason]}" unless reply.success?
   end
@@ -33,14 +47,18 @@ end
 client.subscribe 'animals' do |_ctx, event|
   case event.type
   when :subscribed
+    # When subscription is established (confirmed by Satori RTM)
     puts "Subscribed to the channel: #{event.data[:subscription_id]}"
   when :data
+    # Messages arrive in an array
     event.data[:messages].each { |msg| puts "Animal is received #{msg}" }
   when :error
+    # When a subscribe error occurs
     puts "Subscription error: #{event.data[:error]} -- #{event.data[:reason]}"
   end
 end
 
+# Wait a reply for a subscribe request
 client.wait_all_replies
 
 rnd = Random.new
@@ -57,7 +75,10 @@ loop do
       puts "Failed to publish animal: #{reply.data[:error]} -- #{reply.data[:reason]}"
     end
   end
+
+  # Wait a reply for a publish request
   client.wait_all_replies
 
+  # Read subscription data in a loop from socket for 2 seconds
   client.sock_read_repeatedly duration_in_secs: 2
 end
